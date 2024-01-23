3d92c5907b8c397e397cb77278e4298bdbdd52c8
O_o <2738644273@qq.com>
2024-01-23 09:49:05 +0800

    dds

--- MidiToKeyboard.Application/MainWindow.xaml.cs
+++ MidiToKeyboard.Application/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using MidiToKeyboard.Midi.MidiSong;
 using MidiToKeyboard.Midi.Util;
 using MidiToKeyboard.Application.ViewModel;
 using MidiToKeyboard.Application.Entity;
+using Wpf.Ui.Controls;
 
 namespace MidiToKeyboard.Application
 {
@@ -29,7 +30,7 @@ namespace MidiToKeyboard.Application
         private static IPressKey mPressKey = new PressKeyByWinRing0();
         private MainWindowViewModel MainWindowViewModel { get;  }
         public MainWindow()
-        {
+        { 
             InitializeComponent();
             //初始化按键
             mPressKey.Initialize(EnumWindowsType.Win32);
@@ -65,9 +66,7 @@ namespace MidiToKeyboard.Application
                 midiPlayer?.Dispose();
                 var listbox = e.Source as System.Windows.Controls.ListBox;
                 var data = listbox.SelectedItem as SongView;
-                
-                midiPlayer = new MidiPlayer(data.Song, mPressKey, true);
-
+                midiPlayer = new MidiPlayer(data.Song, mPressKey, OutputDevice.GetByName("Microsoft GS Wavetable Synth"));
             }
             catch (Exception ex)
             {
--- MidiToKeyboard.Midi/MidiSong/MidiPlayer.cs
+++ MidiToKeyboard.Midi/MidiSong/MidiPlayer.cs
@@ -22,19 +22,19 @@ namespace MidiToKeyboard.Midi.MidiSong
         public int ModifiedTone { get; set; } = 0;
         public double Speed { get; set; } = 1d;
         private OutputDevice OutputDevice { get;}
-        public MidiPlayer(Song song, IPressKey pressKey, bool outputAudio)
+        public MidiPlayer(Song song, IPressKey pressKey, OutputDevice outputAudio)
         {
             Song = song;
             Playback = song.GetPlay();
-            if (outputAudio)
+            if (outputAudio is not null)
             {
-                OutputDevice = OutputDevice.GetByName("Microsoft GS Wavetable Synth");
-
-                Playback.OutputDevice = OutputDevice;
+                OutputDevice = outputAudio;
+                Playback.OutputDevice =OutputDevice;
             }
             PressKey = pressKey;
             Playback.NoteCallback = NoteHandler;
         }
+
         private NotePlaybackData NoteHandler(NotePlaybackData data, long time, long length, TimeSpan playbackTime)
         {
             var originalNote = (int)data.NoteNumber;
